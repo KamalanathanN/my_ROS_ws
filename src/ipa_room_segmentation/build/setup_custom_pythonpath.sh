181d5fdbe8199b85453b4b0d59b84a903009bdd8
KamalanathanN <kamal.rkara@gmail.com>
2020-08-29 17:56:39 +0530

    deleted

--- src/ipa_room_segmentation/build/setup_custom_pythonpath.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-# generated from dynamic_reconfigure/cmake/setup_custom_pythonpath.sh.in
-
-PYTHONPATH=/home/kamal/ROSDevs/ROS_DEVS_ws/src/ipa_room_segmentation/build/devel/lib/python2.7/dist-packages:$PYTHONPATH
-exec /usr/bin/python2 "$@"
